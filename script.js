fa77c0a59d035efa61e6d2e04bb10a2c0cd53248
poojaANUP <poojaanup522@gmail.com>
2022-12-27 09:40:19 +0530

    final

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+//EXAMINE THE DOCUMENT OBJECT//
+
+//console.dir(document);
+
+//a//lert(1);
+
+//console.log(document.URL);
+//console.log(document.links);
+
+//var items=document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[2]);
+//items[2].textContent='hello 1';
+//items[2].getElementsByClassName.fontWeight="bold";
+//items[2].getElementsByClassName.backgroundColor='yellow';
+
+var li =document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='hello 2';
